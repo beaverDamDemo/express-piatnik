985797e653b2e4b3e64fd58d9a8e314f8bae013f
beaverDamDemo <zdenevi@gmail.com>
2023-12-10 20:50:53 +0100

    prettified file

--- routes/cars.js
+++ routes/cars.js
@@ -34,126 +34,126 @@ router.post("/about", function(req, res) {
 // });
 
 router.route("/display-all-cars")
-  .get((req, res) => {
-    res.render("all-cars", {
-      carsData: (carInfo.cars)
-    })
+.get((req, res) => {
+  res.render("all-cars", {
+    carsData: (carInfo.cars)
   })
-  .post((req, res) => {
+})
+.post((req, res) => {
     // req.params
     // req.body
     // we need first to parse json from body, but it is already done as we enabled json() in server.js
-    res.send({
-      "req.params": req.params,
-      "req.body": req.body,
-      "carInfo": carInfo
-    })
+  res.send({
+    "req.params": req.params,
+    "req.body": req.body,
+    "carInfo": carInfo
   })
+})
 
 router.route("display-single-car/:id")
-  .get((req, res) => {
-    console.log('REQ', carInfo.cars[req.params.id])
+.get((req, res) => {
+  console.log('REQ', carInfo.cars[req.params.id])
 
-    res.render("single-car", {
-      id: req.params.id,
-      carData: Object.entries(carInfo.cars[req.params.id])
-    })
+  res.render("single-car", {
+    id: req.params.id,
+    carData: Object.entries(carInfo.cars[req.params.id])
   })
-  .put((req, res) => {
-    res.send("put")
-  })
-  .delete((req, res) => {
-    res.send("delete")
-  })
+})
+.put((req, res) => {
+  res.send("put")
+})
+.delete((req, res) => {
+  res.send("delete")
+})
 
 router.route("/statistics/resetSingleCar")
-  .put((req, res) => {
-    client.connect(function(err, db) {
-      if (err || !db) {
-        return err
-      }
+.put((req, res) => {
+  client.connect(function(err, db) {
+    if (err || !db) {
+      return err
+    }
 
-      let _won, _draw, _lost
-      const entries = db.db("piatnik_cars").collection("statistics").find({
-        carName: req.body.carName
-      }).toArray(function(err, result) {
-        if (err) {
-          res.status(400).send("Error fetching listings!");
-        } else {
-          db.db("piatnik_cars").collection("statistics").findOneAndUpdate({
-            carName: req.body.carName
-          }, {
-            $set: {
-              statistics: {
-                won: 0,
-                draw: 0,
-                lost: 0
-              },
-            }
-          }, {
-            upsert: true,
-            returnNewDocument: true
-          })
-        }
-      })
-    });
-    res.json({
-      message: "resetting statistics"
-    })
-  })
-
-router.route("/statistics")
-  .get((req, res) => {
-    res.send("get statistics")
-  })
-  .post((req, res) => {
-    client.connect(function(err, db) {
-      if (err || !db) {
-        return err
-      }
-      db.db("piatnik_cars").collection("statistics").insert({
-        "carName": req.body.carName,
-        "statistics": req.body.statistics
-      })
-    });
-    res.json({
-      message: "post statistics"
-    })
-  })
-
-  .put((req, res) => {
-    client.connect(function(err, db) {
-      if (err || !db) {
-        return err
-      }
-
-      Object.entries(req.body).forEach(entry => {
+    let _won, _draw, _lost
+    const entries = db.db("piatnik_cars").collection("statistics").find({
+      carName: req.body.carName
+    }).toArray(function(err, result) {
+      if (err) {
+        res.status(400).send("Error fetching listings!");
+      } else {
         db.db("piatnik_cars").collection("statistics").findOneAndUpdate({
-          carName: entry[0],
+          carName: req.body.carName
         }, {
-          $inc: {
-            "statistics.won": parseInt(entry[1].duelsWon),
-            "statistics.draw": parseInt(entry[1].duelsTie),
-            "statistics.lost": parseInt(entry[1].duelsLost)
+          $set: {
+            statistics: {
+              won: 0,
+              draw: 0,
+              lost: 0
+            },
           }
         }, {
           upsert: true,
+          returnNewDocument: true
         })
-      })
-    });
-    res.json({
-      message: "put statistics"
-    })
-  })
-  .delete((req, res) => {
-    client.connect(function(err, db) {
-      if (err || !db) {
-        return err
       }
-      db.db("piatnik_cars").collection("statistics").drop()
-      res.send("delete statistics")
     })
+  });
+  res.json({
+    message: "resetting statistics"
   })
+})
+
+router.route("/statistics")
+.get((req, res) => {
+  res.send("get statistics")
+})
+.post((req, res) => {
+  client.connect(function(err, db) {
+    if (err || !db) {
+      return err
+    }
+    db.db("piatnik_cars").collection("statistics").insert({
+      "carName": req.body.carName,
+      "statistics": req.body.statistics
+    })
+  });
+  res.json({
+    message: "post statistics"
+  })
+})
+
+.put((req, res) => {
+  client.connect(function(err, db) {
+    if (err || !db) {
+      return err
+    }
+
+    Object.entries(req.body).forEach(entry => {
+      db.db("piatnik_cars").collection("statistics").findOneAndUpdate({
+        carName: entry[0],
+      }, {
+        $inc: {
+          "statistics.won": parseInt(entry[1].duelsWon),
+          "statistics.draw": parseInt(entry[1].duelsTie),
+          "statistics.lost": parseInt(entry[1].duelsLost)
+        }
+      }, {
+        upsert: true,
+      })
+    })
+  });
+  res.json({
+    message: "put statistics"
+  })
+})
+.delete((req, res) => {
+  client.connect(function(err, db) {
+    if (err || !db) {
+      return err
+    }
+    db.db("piatnik_cars").collection("statistics").drop()
+    res.send("delete statistics")
+  })
+})
 
 router.get("/statistics/:carName", (req, res) => {
   client.connect(function(err, db) {
@@ -176,34 +176,33 @@ router.get("/statistics/:carName", (req, res) => {
 })
 
 router.route("/get-all-cars")
-  .get((req, res) => {
-    res.send({
-      carsData: (carInfo.cars)
-    })
+.get((req, res) => {
+  res.send({
+    carsData: (carInfo.cars)
   })
+})
 
-  router.route("/dummy")
-    .get((req, res) => {
-      res.send({
-        dummyData: [{
-          foo: 'quux',
-          quux: 'foo',
-          stevilkica: 10000000000
-        }]
-      })
-    })
+router.route("/dummy")
+.get((req, res) => {
+  res.send({
+    dummyData: [{
+      foo: 'quux',
+      quux: 'foo',
+      stevilkica: 10000000000
+    }]
+  })
+})
 
 router.route("/get-single-car/:id")
-  .get((req, res) => {
-    res.send({
-      carData: Object.entries(carInfo.cars[req.params.id])
-    })
+.get((req, res) => {
+  res.send({
+    carData: Object.entries(carInfo.cars[req.params.id])
   })
+})
 
 router.route("/get-single-car-image/:id")
-  .get((req, res) => {
-    // console.log(carInfo.cars[req.params.id]["image_url"])
-    res.sendFile(path.join(__dirname, "../public/images/cars/toyota_celica.jpeg"))
-  })
+.get((req, res) => {
+  res.sendFile(path.join(__dirname, "../public/images/cars/toyota_celica.jpeg"))
+})
 
 module.exports = router
